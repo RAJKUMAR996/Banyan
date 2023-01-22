f1a7ee2eccbefe68f41c7537ff3a0cdd8776fb3f
vanithasri-jayabalan <vanithasri.j@gmail.com>
2023-01-22 05:37:38 -0800

    Latest update modal added

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, theme, withGalio, GalioProvider } from 'galio-framework';
+import { StyleSheet, Text, View, Modal } from 'react-native';
+import { Button, theme, withGalio, GalioProvider, Block } from 'galio-framework';
 import AppContainer from './AppNavigator';
+const style = { flex: 1, alignItems: 'center', padding: 15, justifyContent: 'center'};
 
 
 /// Theme reference https://galio.io/docs/#/components/input?id=props
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        show: false
+    }
+}
   render() {
-    return (<GalioProvider theme={{ COLORS: { PRIMARY: '#2e3982', THEME: '#6b88f2' } }}><AppContainer /></GalioProvider>);
+    return (<GalioProvider theme={{ COLORS: { PRIMARY: '#2e3982', THEME: '#6b88f2' } }}><AppContainer />
+    <View style={style}>
+    {/* <Text style={{ fontSize:20}}>Latest Update!</Text> */}
+    <View style={{display: 'flex'}}><Button size="large" title="Click Me" onPress={()=>{this.setState({show:true})}}>Latest Update!</Button></View>
+    <Modal transparent={true}
+    visible={this.state.show}>
+        <View style={{backgroundColor: "#808080", flex:1}}>
+            <View style={{backgroundColor: "#FFD700", margin:50, padding:40, borderRadius:10, flex:1}}>
+                <Text style={{ fontSize:20}}>All of you welcome to New Year Party!</Text>
+                <Button title="Close Me" onPress={()=>{this.setState({show:false})}}>Close Message</Button>
+            </View>
+
+        </View>
+
+    </Modal>
+</View></GalioProvider>
+    );
   }
 }
 
--- src/pages/eventMessage.js
+++ src/pages/eventMessage.js
@@ -1,18 +1,36 @@
 
 import { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Modal } from 'react-native';
 import { Button, theme, withGalio, GalioProvider } from 'galio-framework';
-const style = { flex: 1, alignItems: 'center', padding: 15, justifyContent: 'center', backgroundColor: "#98FB98" };
+const style = { flex: 1, alignItems: 'center', padding: 15, justifyContent: 'center'};
+
 
 class EventMessageScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false
+        }
+    }
     render() {
         return (
             <View style={style}>
-                <Text>Am latest message!</Text>
+                <Text style={{ fontSize:30, width:50, height:50}}>Latest Msg</Text>
+                <Button title="Click Me" onPress={()=>{this.setState({show:true})}}>Open Message</Button>
+                <Modal transparent={true}
+                visible={this.state.show}>
+                    <View style={{backgroundColor: "#FFFFFF", flex:1}}>
+                        <View style={{backgroundColor: "#FFFF00", margin:50, padding:40, borderRadius:10, flex:1}}>
+                            <Text style={{ fontSize:20}}>All of you welcome to New Year Party!</Text>
+                            <Button title="Close Me" onPress={()=>{this.setState({show:false})}}>Close Message</Button>
+                        </View>
+
+                    </View>
+
+                </Modal>
             </View>
+
         );
     }
 }
-
-
 export default EventMessageScreen;
