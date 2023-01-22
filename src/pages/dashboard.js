66611d9d80631cce3626424e92f995b3f7d58d88
Rajkumar Murugarasan <Rajkumar.Murugarasan@cudirect.com>
2023-01-21 23:47:31 -0800

    updating dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,7 @@
 
 import { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, Card, withGalio, GalioProvider } from 'galio-framework';
+import { ScrollView, Text, View } from 'react-native';
+import { Button, Card, theme, Block, styles } from 'galio-framework';
 import { DataHelper } from '../services/firebase.service';
 const style = { padding: 15 };
 
@@ -16,7 +16,6 @@ class HomeScreen extends Component {
     }
     getAllFamilies() {
         new DataHelper().getAll('Families').then(data => {
-            console.log('data', data);
             this.isLoaded = true;
             this.setState({ data });
         });
@@ -27,22 +26,26 @@ class HomeScreen extends Component {
         if (this.isLoaded) {
             data = this.state?.data?.map((family, i) => {
                 return (
-                    <View key={`family-${i}`}>
-                        <Card
-                            style={{ padding: 10, backgroundColor: '#fff' }}
-                        >
-                            <Text>Name: {family.Name}</Text>
-                            <Text>Owner: {family.Owner}</Text>
-                        </Card>
-                    </View>);
+                    <Card key={`test-${i}`}
+                        // borderless
+                        style={{ padding: theme.SIZES.BASE / 2, marginBottom: 20, backgroundColor: '#fff' }}
+                        title={family.Name}
+                        caption={"Owner: " + family.Owner}
+                        avatar={"http://i.pravatar.cc/100?id=pineaple" + i + 1}
+                        // footerStyle={{marginBottom:10}}
+                        imageStyle={{ borderRadius: 10 }}
+                    // image="https://images.unsplash.com/photo-1497802176320-541c8e8de98d?&w=1600&h=900&fit=crop&crop=entropy&q=300"
+                    >
+
+                    </Card>
+                );
             });
         }
         return (
-            <View style={style}>
-                <Text>Welcome to home!</Text>
-                <Button >home</Button>
+            <ScrollView style={style}>
+                <Text style={{marginBottom:20}}>Welcome to home!</Text>
                 {data}
-            </View>
+            </ScrollView>
         );
     }
 }
